e1f385dd369404450c76438d78e2dd272c02e6f0
Qiming Tang <tangqiming@zhiman.cn>
2026-07-09 03:58:02 +0000

    First runnable skeleton

--- projects/01-cli-todo/cli_todo/src/main.rs
+++ projects/01-cli-todo/cli_todo/src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    println!("program: {}", args[0]);
+    if let Err(msg) = run(&args[1..]) {
+        eprintln!("error: {msg}");
+        process::exit(1);
+    }
+}
+
+fn run(args: &[String]) -> Result<(), String> {
+    println!("received: {:?}", args);
+    Ok(())
 }
